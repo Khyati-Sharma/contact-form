130fcf1114f7dc46aa86ff7935b1d462beaec9fb
Khyati Sharma <khyatisharma2001514@gmail.com>
2020-08-20 16:59:40 +0530

    some functionality added

--- contactform.js
+++ contactform.js
@@ -7,12 +7,4 @@ function myFunction(){
     var message = document.getElementById("message").value;
 	
     alert("Your form is submitted, your name is "+fname+" "+lname+" , email - "+email+" , phone- "+phone+"\nmessage subject is "+subject+" - and message is about - "+message)
-    var a =  /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/ ;
-    if(email.value.match(a)){
-    return true ;
-    }
-    else{
-        alert("You have entered an invalid email address!");
-        return false ;
-    }
 }
